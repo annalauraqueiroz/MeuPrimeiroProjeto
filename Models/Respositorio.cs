85de13ef900571b50d0647797c3de326db49f987
Anna Laura Queiroz <annalauraqueiroz@hotmail.com>
2022-07-03 11:23:03 -0300

    Adicionar arquivos de projeto.

---
+++ Models/Respositorio.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MeuPrimeiroProjeto.Models
+{
+    public class Repositorio
+    {
+        private static List<Resposta> respostas = new List<Resposta>();
+        public static IEnumerable<Resposta> Respostas { get { return respostas; } }
+        public static void AdicionarResposta(Resposta resposta)
+        {
+            respostas.Add(resposta);
+        }
+
+        static Repositorio()
+        {
+            respostas.Add(new Resposta()
+            {
+                Nome = "Anna",
+                Email = "anna@gmail.com",
+                Sim = true
+            });
+            respostas.Add(new Resposta()
+            {
+                Nome = "Laura",
+                Email = "laura@gmail.com",
+                Sim = true
+            });
+            respostas.Add(new Resposta()
+            {
+                Nome = "Nancy",
+                Email = "nancy@gmail.com",
+                Sim = true
+            });
+            respostas.Add(new Resposta()
+            {
+                Nome = "Will",
+                Email = "will@gmail.com",
+                Sim = false
+            });
+            respostas.Add(new Resposta()
+            {
+                Nome = "Eleven",
+                Email = "eleven@gmail.com",
+                Sim = true
+            });
+            respostas.Add(new Resposta()
+            {
+                Nome = "Mike",
+                Email = "mike@gmail.com",
+                Sim = false
+            });
+        }
+    }
+}
---
+++ Models/Resposta.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MeuPrimeiroProjeto.Models
+{
+    public class Resposta
+    {
+        [Required(ErrorMessage ="O campo Nome deve ser preenchido")]
+        public string Nome { get; set; }
+        [Required(ErrorMessage = "O campo Email deve ser preenchido")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "O campo Resposta deve ser preenchido")]
+        public bool? Sim { get; set; }
+    }
+}
